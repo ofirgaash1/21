014a5fa57aeb15615a742e1806f9de7ace132f23
Ofir Gaash <77354079+ofirgaash1@users.noreply.github.com>
2025-06-19 15:40:01 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,32 @@
-variable "region" {}
-variable "ami_id" {}
-variable "instance_type" {}
-variable "subnet_id" {}
-variable "security_group_id" {}
-variable "ssh_key_name" {}
-variable "db_subnet_ids" {
-  type = list(string)
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "il-central-1" 
+}
+
+variable "subnet_ids" {
+  description = "List of existing private subnet IDs in the VPC"
+  type        = list(string)
+  default     = [
+    "subnet-xxxxxxxxxxxxxxxx1",
+    "subnet-xxxxxxxxxxxxxxxx2"
+  ]
+}
+
+variable "security_group_id" {
+  description = "ID of an existing security group"
+  type        = string
+  default     = "sg-xxxxxxxxxxxxxxxxx"
+}
+
+variable "key_name" {
+  description = "Name of the existing SSH key pair (not the .pem filename)"
+  type        = string
+  default     = "imtec"
+}
+
+variable "service_name" {
+  description = "Logical name for tagging resources"
+  type        = string
+  default     = "ofir"
 }
